1717a2e9669fa77f53d0fd6738e6b091b6f37afe
anirejlova <anna8080@post.cz>
2025-12-09 11:33:03 +0000

    create header as we have it and implement in order_screen

---
+++ lib/views/widgets.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/cart.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
+import 'package:sandwich_shop/views/cart_screen.dart';
+
+class AppBarHeader extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const AppBarHeader({
+    Key? key,
+    this.title = 'Sandwich Counter',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          height: 100,
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
+      title: Text(
+        title,
+        style: heading1,
+      ),
+      actions: [
+        Consumer<Cart>(
+          builder: (context, cart, child) {
+            return IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CartScreen(cart: cart),
+                  ),
+                );
+              },
+              icon: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.shopping_cart),
+                  const SizedBox(width: 4),
+                  Text('${cart.countOfItems}'),
+                ],
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
